df883aa155026d667a616d6a99b33058469b5e93
rrzaevich <47025875+nevidomyi@users.noreply.github.com>
2023-05-06 19:15:31 +0300

    Has been added Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <Router>
+        <Navbar />
+        <main>{/* <Routes></Routes> */}</main>
+      </Router>
+    </ChakraProvider>
+  );
 }
 
 export default App;
